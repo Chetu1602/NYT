1a6336b5feff96cebcd59170117fd94c6c40ab83
Chetu <raipureddy.chetana@gmail.com>
2024-03-26 22:41:30 +0530

    removing strict mode

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,66 +1,34 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// interface newsProp{
-//     news:any
-//     num:any
-// }
-// const Home = (props:newsProp) => {
-  
-//   return (
-//     <div className='homeContainer contentStyle w-[77%]'>
-//     <div>
-//         <Link to="/details" state={{data:props?.news[`${props?.num}`]}}><div className='flex'>
-//             <div>
-//             <h1 className='font-bold text-xl font-serif'>{props?.news[`${props.num}`]?.title}</h1>
-//             <p className='text-sm font-serif'>{props?.news[`${props.num}`]?.content}</p>
-//             </div>
-//             <img className='w-[63%] h-[22rem] ml-7'src={props?.news[`${props.num}`]?.urlToImage} alt="Img" />
-//         </div>
-//         </Link>
-//     </div>
-//     <div className="borderBottom"></div>
-
-//     </div>
-    
-//   )
-  
-// }
-
-
-// export default Home
-
-
-
-// new
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 interface newsProp{
-      news:any
-      num:any
-   }
-const Home = (props: newsProp) => {
-  if (!props.news || props.news.length === 0 || !props.news[props.num]) {
-    return <div>No news available</div>;
-  }
-
+    news:any
+    num:any
+}
+const Home = (props:newsProp) => {
+  
   return (
     <div className='homeContainer contentStyle w-[77%]'>
-      <div>
-        <Link to="/details" state={{ data: props.news[props.num] }}>
-          <div className='flex'>
+    <div>
+        <Link to="/details" state={{data:props?.news[`${props?.num}`]}}><div className='flex'>
             <div>
-              <h1 className='font-bold text-xl font-serif'>{props.news[props.num]?.title}</h1>
-              <p className='text-sm font-serif'>{props.news[props.num]?.content}</p>
+            <h1 className='font-bold text-xl font-serif'>{props?.news[`${props.num}`]?.title}</h1>
+            <p className='text-sm font-serif'>{props?.news[`${props.num}`]?.content}</p>
             </div>
-            <img className='w-[63%] h-[22rem] ml-7' src={props.news[props.num]?.urlToImage} alt="Img" />
-          </div>
+            <img className='w-[63%] h-[22rem] ml-7'src={props?.news[`${props.num}`]?.urlToImage} alt="Img" />
+        </div>
         </Link>
-      </div>
-      <div className="borderBottom"></div>
     </div>
-  );
-};
+    <div className="borderBottom"></div>
+
+    </div>
+    
+  )
+  
+}
+
 
 export default Home
+
+
+
